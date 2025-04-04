64b2f46cfd7bc7626f32bf4c38cfb6de2683a40a
Trieu <huynhquangtrieu1006@gmail.com>
2025-04-04 21:19:56 +0700

    Remove public/css folder

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+// route
+app.get('/', (req, res) => {
+  res.render('home'); //render home sẽ đưa vào body
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
